a170a05658c3f47768cc0d9dae6aeb813509f19a
love2007-web <adeoluwaloveowolabi@gmail.com>
2024-04-24 10:09:10 +0100

    Update signup component HTML with new design and signup options

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Pages/home/home.component';
 import { SignupComponent } from './Pages/signup/signup.component';
+import { UserSignupComponent } from './Pages/user-signup/user-signup.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'user_signup', component: UserSignupComponent }
+
 
 ];
 
