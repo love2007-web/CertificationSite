000564591403b1b48ab5017538017c1bf63f2575
love2007-web <adeoluwaloveowolabi@gmail.com>
2024-07-13 17:05:35 +0100

    chore: Update organization navigation component to toggle sidebar icon based on expanded state

--- src/app/Components/side-nav/side-nav.component.ts
+++ src/app/Components/side-nav/side-nav.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SidebarService } from '../../Services/signup.service';
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrl: './side-nav.component.css'
 })
-export class SideNavComponent {
+export class SideNavComponent implements OnInit {
+  isExpanded: boolean = true;
 
+  menuItems = [
+    { icon: 'home', text: 'Home' },
+    { icon: 'person', text: 'Profile' },
+    { icon: 'settings', text: 'Settings' },
+    // Add more menu items as needed
+  ];
+
+  constructor(private sidebarService: SidebarService) {}
+
+  ngOnInit() {
+    this.sidebarService.isExpanded$.subscribe(isExpanded => {
+      this.isExpanded = isExpanded;
+    });
+  }
 }
