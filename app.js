2b5724610d4171b809338558cbc998ff02d7d3bf
love2007-web <adeoluwaloveowolabi@gmail.com>
2024-11-27 21:49:24 +0100

    Update CORS policy to restrict access to specific origin

--- app.js
+++ app.js
@@ -19,7 +19,10 @@ const dbURI = process.env.dbURI;
 
 // CORS Headers
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://blaze-certify.netlify.app"
+  );
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization,auth-token");
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
